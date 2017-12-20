81d4fb65b79ceffba7f246b4c6d6570237b64f1b
thingsareright <2271366490@qq.com>
2017-12-20 18:01:23 +0800

    对原来的商品列表界面进行了模糊查询的优化。 添加了初始化数据的路径，但是之后还要把boss表的id设置为可以为NULL，否则会报主键没有默认值。 算了，明天中午再写说明。

--- src/main/java/com/example/demo/dao/BookRepository.java
+++ src/main/java/com/example/demo/dao/BookRepository.java
@@ -11,7 +11,8 @@ import java.util.List;
  * Created by Administrator on 2017/12/8 0008.
  */
 public interface BookRepository extends JpaRepository<Book, Long> {
-    @Query(value = "SELECT * FROM book  where bookname like CONCAT('%',?1,'%')  limit ?2,?3", nativeQuery = true)
+
+    @Query(value = "SELECT * FROM book WHERE id > (SELECT id FROM book WHERE bookname like CONCAT('%',?1,'%') LIMIT ?2,1) LIMIT ?3", nativeQuery = true)
     List<Book> mySearch( String bookname,int begin, int range);
 
     Book findById(int id);
