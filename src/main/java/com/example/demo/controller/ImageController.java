4147203ba981597dde5523b2c34fc6c1a2583463
thingsareright <2271366490@qq.com>
2017-12-29 15:20:11 +0800

    把短信验证码的功能加了上去。                                                                                                                                                                                     建好了订单表。                                                                                                                                                                                     完成了订单表的数据返回。                                                                                                                                                                                     上传了一些用于向数据库建立触发器的sql语句。                                                                                                                                                                                     实现了User表，而且还完善了修改和查看个人信息的接口。                                                                                                                                                                                     上传图片接口。

---
+++ src/main/java/com/example/demo/controller/ImageController.java
@@ -0,0 +1,82 @@
+package com.example.demo.controller;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@RestController
+@RequestMapping("/image")
+public class ImageController {
+
+    public static final String ROOT = "upload-dir";
+
+    private final ResourceLoader resourceLoader;
+
+    @Autowired
+    public ImageController(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/")
+    public String provideUploadInfo(Model model) throws IOException {
+
+        model.addAttribute("files", Files.walk(Paths.get(ROOT))
+                .filter(path -> !path.equals(Paths.get(ROOT)))
+                .map(path -> Paths.get(ROOT).relativize(path))
+                .map(path -> linkTo(methodOn(ImageController.class).getFile(path.toString())).withRel(path.toString()))
+                .collect(Collectors.toList()));
+
+        return "uploadForm";
+    }
+
+    //显示图片的方法关键 匹配路径像 localhost:8080/b7c76eb3-5a67-4d41-ae5c-1642af3f8746.png
+    @RequestMapping(method = RequestMethod.GET, value = "/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<?> getFile(@PathVariable String filename) {
+
+        try {
+            return ResponseEntity.ok(resourceLoader.getResource("file:" + Paths.get(ROOT, filename).toString()));
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    //上传的方法
+    @RequestMapping(method = RequestMethod.POST, value = "/")
+    public String handleFileUpload(@RequestParam("file") MultipartFile file,
+                                   RedirectAttributes redirectAttributes, HttpServletRequest request) {
+        System.out.println("******" + file.getName());  // TODO: 2017/12/29
+        System.out.println(request.getParameter("member"));
+        if (!file.isEmpty()) {
+            try {
+                Files.copy(file.getInputStream(), Paths.get(ROOT, file.getOriginalFilename()));
+                redirectAttributes.addFlashAttribute("message",
+                        "You successfully uploaded " + file.getOriginalFilename() + "!");
+            } catch (IOException|RuntimeException e) {
+                redirectAttributes.addFlashAttribute("message", "Failued to upload " + file.getOriginalFilename() + " => " + e.getMessage());
+            }
+        } else {
+            redirectAttributes.addFlashAttribute("message", "Failed to upload " + file.getOriginalFilename() + " because it was empty");
+        }
+
+        return "redirect:/";
+    }
+
+}
